b714ccb001c590f82574f06e3d1cc1494954efe8
Guilherme Kipper <33471042+guikipper@users.noreply.github.com>
2023-08-23 16:50:41 +0000

    atualizando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() { //nome maiúsculo para componente
-    return <h1>Alteração</h1>
+    return <h1>Amor, quero te ver hoje, não to muito bem das ideias e tu sempre me faz muito bem!</h1>
 }
 
 export default Home;
