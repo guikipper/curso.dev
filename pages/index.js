500a7f436d6f440fa3e06fb93dbc395528d93e2d
Guilherme Kipper <33471042+guikipper@users.noreply.github.com>
2023-08-25 14:27:40 +0000

    adiciona script  e

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
-function Home() { //nome maiúsculo para componente
-    return <h1>Amor, quero te ver hoje, não to muito bem das ideias e tu sempre me faz muito bem!</h1>
+function Home() {
+  //nome maiúsculo para componente
+  return (
+    <h1>
+      Amor, quero te ver hoje, não to muito bem das ideias e tu sempre me faz
+      muito bem!
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("lalal");
 }
 
 export default Home;
